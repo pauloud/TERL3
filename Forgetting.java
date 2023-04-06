67f68e22a681808470af2cf110b80dcd40bc328b
pauloud <57322619+pauloud@users.noreply.github.com>
2023-04-06 15:25:22 +0200

    sauvegarde version non testée de l'algo

--- Forgetting.java
+++ Forgetting.java
@@ -1,29 +1,40 @@
-import fr.lirmm.graphik.graal.api.core.ConjunctiveQuery;
-import fr.lirmm.graphik.graal.api.core.Rule;
-import fr.lirmm.graphik.graal.api.core.RuleSet;
+import fr.lirmm.graphik.graal.api.core.*;
 import fr.lirmm.graphik.graal.core.DefaultConjunctiveQuery;
 import fr.lirmm.graphik.graal.core.compilation.NoCompilation;
-import fr.lirmm.graphik.graal.core.grd.*;
 import fr.lirmm.graphik.graal.core.ruleset.IndexedByHeadPredicatesRuleSet;
-import fr.lirmm.graphik.graal.core.ruleset.LinkedListRuleSet;
 import fr.lirmm.graphik.graal.core.unifier.AtomicHeadRule;
 import fr.lirmm.graphik.graal.core.unifier.QueryUnifier;
 import fr.lirmm.graphik.graal.core.unifier.UnifierUtils;
 
-import java.util.Iterator;
 import java.util.LinkedList;
 
 public class Forgetting {
-   public static RuleSet fermeture (Iterable<AtomicHeadRule> rInit){
-       Iterable<AtomicHeadRule> rNouvelles =  rInit;
-       while (rNouvelles.iterator().hasNext()){
+   public static RuleSet fermeture (RuleSet rInit){
+       RuleSet rFerme = rInit;
+       RuleSet rNouvelles =  rInit;
+       while (! rNouvelles.isEmpty()){
            RuleSet rProduit = new IndexedByHeadPredicatesRuleSet();
-           for (AtomicHeadRule r1:rNouvelles)
+           for (Rule r1: rNouvelles) {
                for (Rule r0 : rInit) {
                    ConjunctiveQuery tete = new DefaultConjunctiveQuery(r0.getHead());
-                   LinkedList<QueryUnifier> unificateurs = UnifierUtils.getSinglePieceUnifiersAHR(tete, r1, NoCompilation.instance());
+                   LinkedList<QueryUnifier> unificateurs = UnifierUtils.getSinglePieceUnifiersAHR(tete, (AtomicHeadRule) r1, NoCompilation.instance());
+                   for (QueryUnifier unificateur : unificateurs) {
+                       Substitution subst = unificateur.getAssociatedSubstitution();
+                       Atom nouvelleTete = subst.createImageOf(r1.getHead()).iterator().next();
+                       InMemoryAtomSet nouveauCorps = subst.createImageOf(r1.getBody());
+                       nouveauCorps.addAll(subst.createImageOf(r0.getBody()));
+                       nouveauCorps.removeAll(subst.createImageOf(r0.getHead()));
+                       rProduit.add(new AtomicHeadRule(nouveauCorps, nouvelleTete));
+
+                   }
                }
+           }
+           rFerme.addAll(rProduit.iterator());
+           rNouvelles = rProduit;
+
+
+
        }
-       return null;
+       return rFerme;
    }
 }
